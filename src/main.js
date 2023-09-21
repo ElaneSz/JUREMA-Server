83474c1d7ba17ec773f7ffd3a08655c94a5e4a1e
Your Name <you@example.com>
2023-09-21 10:14:51 -0300

    refatorando as rotas

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const app = express();
-const rotaMedicao = require('./routes/medicao');
-const port = 3000;
+const express = require('express')
+const app = express()
+const rotaMedicao = require('./routes/medicao')
+const port = 3000
 
-app.use(express.json());
-app.use('/medicao', rotaMedicao);
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
-
-// oi
+app.use('/', express.static(__dirname + '/../public'))
+app.use('/api', rotaMedicao)
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
--- src/routes/medicao.js
+++ src/routes/medicao.js
@@ -19,6 +19,19 @@ db.serialize(() => {
   );
 });
 
+
+router.get('/', (req, res) => {
+  // Consulta para selecionar todos os dados da tabela "medicao"
+  const query = 'SELECT * FROM medicao';
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error('Erro ao executar a consulta:', err);
+      return res.status(500).send('Erro no Servidor Interno');
+    }
+    res.json(rows);
+  })
+})
+
 // Endpoint for receiving humidity data
 router.post('/', (req, res) => {
   const { umidade, temperatura, ninho } = req.body;
@@ -36,89 +49,89 @@ router.post('/', (req, res) => {
 });
 
 
-// Endpoint para exibir os dados em uma tabela HTML
-router.get('/tabela', (req, res) => {
-  // Consulta para selecionar todos os dados da tabela "medicao"
-  const query = 'SELECT * FROM medicao';
+// // Endpoint para exibir os dados em uma tabela HTML
+// router.get('/tabela', (req, res) => {
+//   // Consulta para selecionar todos os dados da tabela "medicao"
+//   const query = 'SELECT * FROM medicao';
 
-  db.all(query, (err, rows) => {
-    if (err) {
-      console.error('Erro ao executar a consulta:', err);
-      return res.status(500).send('Erro no Servidor Interno');
-    }
+//   db.all(query, (err, rows) => {
+//     if (err) {
+//       console.error('Erro ao executar a consulta:', err);
+//       return res.status(500).send('Erro no Servidor Interno');
+//     }
 
-    // Renderiza os dados em uma tabela HTML
-    const htmlTable = `
-      <html>
-        <head>
-          <title>Tabela de Medição</title>
-          <style>
-            /* Estilo para a tabela */
-            table {
-                border-collapse: collapse;
-                width: 100%;
-                border: 1px solid #ddd;
-                font-family: Arial, sans-serif;
-            }
+//     // Renderiza os dados em uma tabela HTML
+//     const htmlTable = `
+//       <html>
+//         <head>
+//           <title>Tabela de Medição</title>
+//           <style>
+//             /* Estilo para a tabela */
+//             table {
+//                 border-collapse: collapse;
+//                 width: 100%;
+//                 border: 1px solid #ddd;
+//                 font-family: Arial, sans-serif;
+//             }
 
-            /* Estilo para as células de cabeçalho */
-            th {
-                background-color: #f2f2f2;
-                text-align: left;
-                padding: 8px;
-            }
+//             /* Estilo para as células de cabeçalho */
+//             th {
+//                 background-color: #f2f2f2;
+//                 text-align: left;
+//                 padding: 8px;
+//             }
 
-            /* Estilo para as células de dados */
-            td {
-                text-align: left;
-                padding: 8px;
-                border-bottom: 1px solid #ddd;
-            }
+//             /* Estilo para as células de dados */
+//             td {
+//                 text-align: left;
+//                 padding: 8px;
+//                 border-bottom: 1px solid #ddd;
+//             }
 
-            /* Estilo para células de dados alternadas (para listas zebradas) */
-            tr:nth-child(even) {
-                background-color: #f2f2f2;
-            }
+//             /* Estilo para células de dados alternadas (para listas zebradas) */
+//             tr:nth-child(even) {
+//                 background-color: #f2f2f2;
+//             }
 
-            /* Estilo para células de dados quando passa o mouse por cima */
-            tr:hover {
-                background-color: #ddd;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>Tabela de Medição</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Umidade</th>
-                <th>Temperatura</th>
-                <th>Ninho</th>
-                <th>Timestamp</th>
-              </tr>
-            </thead>
-            <tbody>
-              ${rows.map((row) => `
-                <tr>
-                  <td>${row.id}</td>
-                  <td>${row.umidade}</td>
-                  <td>${row.temperatura}</td>
-                  <td>${row.ninho}</td>
-                  <td>${row.timestamp}</td>
-                </tr>
-              `).join('')}
-            </tbody>
-          </table>
-        </body>
-        <script>
-          setTimeout(() => window.location.reload(), 1000)
-        </script>
-      </html>
-    `;
-    res.send(htmlTable);
-  });
-});
+//             /* Estilo para células de dados quando passa o mouse por cima */
+//             tr:hover {
+//                 background-color: #ddd;
+//             }
+//           </style>
+//         </head>
+//         <body>
+//           <h1>Tabela de Medição</h1>
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>ID</th>
+//                 <th>Umidade</th>
+//                 <th>Temperatura</th>
+//                 <th>Ninho</th>
+//                 <th>Timestamp</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               ${rows.map((row) => `
+//                 <tr>
+//                   <td>${row.id}</td>
+//                   <td>${row.umidade}</td>
+//                   <td>${row.temperatura}</td>
+//                   <td>${row.ninho}</td>
+//                   <td>${row.timestamp}</td>
+//                 </tr>
+//               `).join('')}
+//             </tbody>
+//           </table>
+//         </body>
+//         <script>
+//           setTimeout(() => window.location.reload(), 1000)
+//         </script>
+//       </html>
+//     `;
+//     res.send(htmlTable);
+//   });
+// });
 
 
 //setTimeout(() => window.location.reload(), 1000)
